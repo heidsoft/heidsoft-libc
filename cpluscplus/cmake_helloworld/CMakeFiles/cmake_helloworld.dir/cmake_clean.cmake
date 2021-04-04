d308ce2c1cc775b7ab90b93f1dbcb0d5eb03fe81
heidsoft <heidsoft@qq.com>
2021-04-04 23:33:02 +0800

    codelite dev cpp test

---
+++ cpluscplus/cmake_helloworld/CMakeFiles/cmake_helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_helloworld.dir/src/Main.cpp.o"
+  "cmake_helloworld"
+  "cmake_helloworld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpluscplus/cmake_helloworld/src/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    
+    std::cout<<"helloworld"<< std::endl;
+    std::cin.get();
+    return 0;
+}
